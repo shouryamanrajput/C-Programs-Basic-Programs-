410f3afac39059613fbb6e6c46b2bcc7929ed80c
Shouryaman Singh Langeh <122371427+shouryamanrajput@users.noreply.github.com>
2023-08-09 08:05:50 -0700

    Add files via upload

---
+++ C Prog - Basic Codes/Q3. Take an integer as input and print half of the number..cpp
@@ -0,0 +1,14 @@
+// Question Take an integer as input and print half of the number.
+
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the number :");
+	scanf("%d",&a);
+	
+	float half = a/2.0;
+	printf("Half of the number is : %f", half);
+	return 0;
+	
+}
